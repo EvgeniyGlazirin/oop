f374c97e448163de82a0263a7e2c64d0edf066dd
EvgeniyGlazirin <technoglazirin@gmail.com>
2018-02-16 23:17:59 +0300

    added lan1.1

---
+++ copyfile/copyfile/test.bat
@@ -0,0 +1,28 @@
+rem %1 - значение первого аргумента командной строки bat-файла (какой он есть)
+rem %~1 - значение первого аргумента командной строки bat-файла с удалением обрамляющих кавычек (если они были)
+
+rem Переменная PROGRAM будет хранить первый аргумент командной строки заключённый в кавычки
+set PROGRAM="%~1" 
+
+echo Testing of number arguments
+%PROGRAM% input.txt output.txt 
+if ERRORLEVEL 1 goto err
+
+echo Testing copying of empty file
+%PROGRAM% empty.txt %TEMP%\empty.txt
+if ERRORLEVEL 1 goto err
+fc.exe %TEMP%\empty.txt empty.txt
+if ERRORLEVEL 1 goto err
+
+echo Testing copying not empty file
+%PROGRAM% input.txt %TEMP%\output.txt
+if ERRORLEVEL 1 goto err
+fc.exe %TEMP%\output.txt input.txt
+if ERRORLEVEL 1 goto err
+
+echo Sucsess testing
+exit 0
+
+:err
+echo Program testing failed
+exit 1
