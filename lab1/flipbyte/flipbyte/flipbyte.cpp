efc7b008bfcb3e42c9c15a0f820b3be4645dc620
EvgeniyGlazirin <technoglazirin@gmail.com>
2018-03-16 15:51:34 +0300

    добавлена программа flipbyte

---
+++ lab1/flipbyte/flipbyte/flipbyte.cpp
@@ -0,0 +1,67 @@
+﻿// flipbyte.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <string>
+
+uint8_t FlipByte(uint8_t inputByte)
+{
+	uint8_t outputByte;
+
+	outputByte = ((inputByte & 0b11110000) >> 4) | ((inputByte & 0b00001111) << 4);
+	outputByte = ((outputByte & 0b11001100) >> 2) | ((outputByte & 0b00110011) << 2);
+	outputByte = ((outputByte & 0b10101010) >> 1) | ((outputByte & 0b01010101) << 1);
+
+	return outputByte;
+}
+
+int StringToInteger(char* input) 
+{
+	try
+	{
+		int inputNumber = std::stoi(input);
+		return (inputNumber);
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+		return 1;
+	}
+}
+
+bool IsInputDataCorrect(int inputNumber) 
+{
+	bool CheckInputDataCorrection = true;
+	if ((inputNumber < 0) || (inputNumber > 255))
+	{
+		std::cout << "Invalid argument value. It must be beetwen 0 and 255. \n";
+		CheckInputDataCorrection = false;
+	}
+	return CheckInputDataCorrection;
+}
+
+
+
+int main(int argc, char* argv[] )
+{
+	if (argc != 2)
+	{
+		std::cout << "Invalid count of arguments.\n" <<
+			"Usage: flipbyte.exe <input byte> \n";
+		return 1;
+	}
+
+	int inputNumber = StringToInteger(argv[1]);
+
+	if (!IsInputDataCorrect(inputNumber))
+	{
+		return 1;
+	}
+
+	int outputNumber = FlipByte(inputNumber);
+
+	std::cout << outputNumber << "\n";
+
+    return 0;
+}
+
---
+++ lab1/flipbyte/flipbyte/test.bat
@@ -0,0 +1,48 @@
+﻿rem %1 - значение первого аргумента командной строки bat-файла (какой он есть)
+rem %~1 - значение первого аргумента командной строки bat-файла с удалением обрамляющих кавычек (если они были)
+
+rem Переменная PROGRAM будет хранить первый аргумент командной строки заключённый в кавычки
+set PROGRAM="%~1"
+
+rem Тесты корректности вводимых аргументов и их количества
+echo Test1 - Testing launching without arguments
+%PROGRAM% > nul
+if not ERRORLEVEL 1 goto err
+
+echo Test2 - Testing launching with right parameters count
+%PROGRAM% 1 >nul
+if ERRORLEVEL 1 goto err
+
+echo Test3 - Testing launching with wrong parameters count
+%PROGRAM% 1 2 > nul
+if not ERRORLEVEL 1 goto err
+
+echo Test4 - Testing launching with right parameters count and wrong data(not digit)
+%PROGRAM% notDigit >nul 
+if ERRORLEVEL 1 goto err
+
+echo Test5 - Testing launching with wrong data (max value)
+%PROGRAM% 256 > nul
+if not ERRORLEVEL 1 goto err
+
+echo Test6 - Testing launching with wrong data (min value)
+%PROGRAM% -1 >nul
+if not  ERRORLEVEL 1 goto err
+
+rem Тесты сравнения ожидаемого результата с получившимся
+echo Test7 - Testing result with expected result
+%PROGRAM% 6 > "%TEMP%\result.txt"
+fc.exe "%TEMP%\result.txt" expectedResult.txt >nul
+if ERRORLEVEL 1 goto err
+
+echo Test8 - Testing result with expected wrong result
+%PROGRAM% 33 > "%TEMP%\result.txt"
+fc.exe "%TEMP%\result.txt" expectedWrongResult.txt >nul
+if not ERRORLEVEL 1 goto err
+
+echo Testing sucsessful!
+exit 0
+
+:err
+echo Program testing failed
+exit 1
