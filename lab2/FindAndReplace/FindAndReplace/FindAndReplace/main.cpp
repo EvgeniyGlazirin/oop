550f8cbb9c6bfb4bae94c4b0457bc9296fcba2fb
EvgeniyGlazirin <technoglazirin@gmail.com>
2018-04-13 18:10:58 +0300

    добаваил программу FindAndReplace

---
+++ lab2/FindAndReplace/FindAndReplace/FindAndReplace/main.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		std::cout << "Invalid count of arguments." << std::endl
+			<< "Usage: FindAndReplace.exe <input string> <search string> <replace string>" 
+			<< std::endl;
+		return 1;
+	}
+	std::string subject;
+	std::getline(std::cin, subject);
+	std::cout << FindAndReplace(subject, argv[2], argv[3]);
+
+	return 0;
+}
