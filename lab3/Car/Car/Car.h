86a5e4b2407fcc0b2273ac70ba6ee7cf1f0bb1d2
EvgeniyGlazirin <technoglazirin@gmail.com>
2018-05-04 22:46:24 +0300

    начало car

---
+++ lab3/Car/Car/Car.h
@@ -0,0 +1,45 @@
+#pragma once
+
+class CCar
+{
+public:
+	bool IsEngineTurnedOn() const;
+	bool TurnOnEngine();
+	bool TurnOffEngine();
+	
+	bool SetGear(int gear);
+	int GetGear()const;
+
+	bool SetSpeed(int speed);
+	int GetSpeed()const;
+
+
+	enum class Direction; 
+	Direction GetDirection() const;
+	
+private:
+	bool m_engineIsTurnedOn = false;
+	int m_gear = 0;
+	int m_speed = 0;
+	enum class Gear;
+
+};
+
+enum class CCar::Gear
+{
+	Reverse = -1,
+	Neutal = 0,
+	First = 1,
+	Second = 2,
+	Third = 3,
+	Fourth = 4,
+	Fifth = 5
+};
+
+
+enum class CCar::Direction
+{
+	Back,
+	Stop,
+	Forward
+};
