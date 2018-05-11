fb831f63f30091e5ea943d78f5c4e0184543bfe7
EvgeniyGlazirin <technoglazirin@gmail.com>
2018-05-11 17:41:22 +0300

    car v2

--- lab3/Car/Car/Car.h
+++ lab3/Car/Car/Car.h
@@ -6,23 +6,17 @@ public:
 	bool IsEngineTurnedOn() const;
 	bool TurnOnEngine();
 	bool TurnOffEngine();
-	
 	bool SetGear(int gear);
 	int GetGear()const;
-
 	bool SetSpeed(int speed);
 	int GetSpeed()const;
-
-
 	enum class Direction; 
 	Direction GetDirection() const;
-	
 private:
 	bool m_engineIsTurnedOn = false;
 	int m_gear = 0;
 	int m_speed = 0;
 	enum class Gear;
-
 };
 
 enum class CCar::Gear
@@ -35,7 +29,6 @@ enum class CCar::Gear
 	Fourth = 4,
 	Fifth = 5
 };
-
 
 enum class CCar::Direction
 {
